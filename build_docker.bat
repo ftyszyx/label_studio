51978283306cf43a1bc7acd545166d285fa89dcb
ftyszyx <whyzi@qq.com>
2024-12-29 10:08:40 +0800

    init

--- build_docker.bat
+++ build_docker.bat
@@ -1,3 +1,2 @@
 
-@REM docker build -t heartexlabs/label-studio-zyx . --debug --progress=plain
-docker build -t label-studio-zyx . --progress=plain
+docker build -t zyx/label-studio:latest . --progress=plain
---
+++ push_docker.bat
@@ -0,0 +1,1 @@
+docker push zyx/label-studio:latest
